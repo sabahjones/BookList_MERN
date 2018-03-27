8a0077e90ab0aa74a73d65b3eda2b693681fc078
sabahjones <sabahjones@gmail.com>
2018-03-27 08:39:11 -0500

    updated server config

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://sabah:password@ds125479.mlab.com:25479/heroku_z2s42j78",
   {
     useMongoClient: true
   }
